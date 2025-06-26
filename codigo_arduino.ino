db5425234602788b977221a2cd8115645c50c8f3
Rafael Pim Santos <113942672+trezepim@users.noreply.github.com>
2025-06-26 12:02:52 -0300

    Final commit

--- codigo_arduino.ino
+++ codigo_arduino.ino
@@ -1,49 +1,47 @@
-#include <Servo.h> // Inclui a biblioteca para controlar o servo motor
+#include <Servo.h>
 
-const int pinoLDR = A0;   // Pino analógico onde o sensor de luz está conectado
-const int pinoServo = 9;  // Pino digital PWM onde o fio de sinal do servo está conectado
+const int pinoLDR = A0;   // pino onde o sensor de luz está conectado
+const int pinoServo = 9;  // pino onde o fio de sinal do servo está conectado
 
-// 90 graus = persiana abaixada e 0 é persianas levantadas
 const int posBaixada = 90;
 const int posLevantada = 0;
 
-// --- Limite de Luz ---
-// valor que define quando está "claro" o suficiente para baixar a persiana
+// limite de luz
+// valor que define quando está claro o suficiente para baixar a persiana
 const int limiarLuz = 500;
 
-// cria um objeto 'servo'
+// cria o objeto servo
 Servo meuServo;
 
 void setup() {
   Serial.begin(9600);
 
-  // Associa o nosso objeto servo ao pino físico no Arduino
+  // associa o objeto servo ao pino físico no arduino
   meuServo.attach(pinoServo);
 
-  // Inicia com a persiana levantada
+  // inicia com a persiana levantada
   meuServo.write(posLevantada);
   Serial.println("Sistema iniciado. Persiana levantada.");
 }
 
 void loop() {
-  // 1. Lê o valor do sensor de luz
+  // lê o valor do sensor de luz
   int valorLuz = analogRead(pinoLDR);
 
-  // 2. Mostra o valor no Monitor Serial (muito útil para calibrar o 'limiarLuz')
+  // mostra o valor no monitor serial
   Serial.print("Valor do sensor de luz: ");
   Serial.println(valorLuz);
 
-  // 3. LÓGICA PRINCIPAL - AQUI ESTÁ A MUDANÇA
   if (valorLuz > limiarLuz) {
-    // Se a luz for FORTE (acima do limiar), a persiana é ATIVADA e ABAIXA
+    // se a luz for acima do limiar, a persiana abaixa
     meuServo.write(posBaixada);
     Serial.println("Ambiente claro. Baixando a persiana...");
   } else {
-    // Se a luz for FRACA (abaixo do limiar), a persiana fica DESLIGADA (ou seja, levantada)
+    // Se a luz for abaixo do limiar, a persiana levanta
     meuServo.write(posLevantada);
     Serial.println("Ambiente escuro. Mantendo a persiana levantada.");
   }
 
-  // Espera um tempo antes de verificar a luz novamente para não ficar movendo o tempo todo
-  delay(1000); // Pausa de 1 segundo
+  // delay de 1 segundo para verificar a luz novamente
+  delay(1000);
 }
